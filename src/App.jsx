57d83099250994ac3be004d53a7312bb2f826ea6
Gökhan Duman <gokhan.duman@wsrlife.com>
2023-09-07 18:22:06 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "devextreme-react";
+
+function App() {
+  return (
+    <>
+      <Button text="Click me" onClick={(val) => console.log(val)} />
+    </>
+  );
+}
+
+export default App;
