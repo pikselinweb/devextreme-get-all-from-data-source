92f7feb2f6dc82aeb363aac2a4127d553fd13073
Gökhan Duman <gokhan.duman@wsrlife.com>
2023-09-14 16:37:56 +0300

    add datagrid

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,59 @@
-import { Button } from "devextreme-react";
+import { useMemo, useRef } from "react";
+// DEVEXTREME
+import { Button } from "devextreme-react/button";
+import DataGrid, {
+  Editing,
+  Item,
+  Scrolling,
+  Toolbar,
+} from "devextreme-react/data-grid";
+// STYLES
 import "./App.css";
+
 function App() {
+  // GRID REF
+  const gridRef = useRef(null);
+  // PRODUCT SAMPLE DATA
+  const productList = useMemo(() => {
+    return [...Array(1000).keys()].map((itm) => ({
+      id: itm + 1,
+      title: `Product ${itm + 1}`,
+      price: Math.floor(Math.random() * 100),
+    }));
+  }, []);
+
   return (
     <>
-      <Button text="Click me" onClick={(val) => console.log(val)} />
+      <div className="grid-wrapper">
+        {/* DATAGRID */}
+        <DataGrid
+          ref={gridRef}
+          dataSource={productList}
+          rowAlternationEnabled={true}
+          showBorders={true}
+          width="100%"
+          height="100%"
+        >
+          {/* TOOLBAR */}
+          <Toolbar>
+            {/* SAVE BUTTON */}
+            <Item location="before">
+              <Button icon="save" type="default" stylingMode="contained" />
+            </Item>
+            {/* NEW ROW BUTTON */}
+            <Item name="addRowButton" />
+          </Toolbar>
+          {/* EDITING */}
+          <Editing
+            allowUpdating={true}
+            allowAdding={true}
+            allowDeleting={true}
+            mode="cell"
+          />
+          {/* VIRTUAL SCROLLING */}
+          <Scrolling mode="virtual" />
+        </DataGrid>
+      </div>
     </>
   );
 }
